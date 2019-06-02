271445a4e1779ed34eb1ca8bf79a3d1f1dafca5a
1145054472@qq.com <1145054472@qq.com>
2019-06-02 23:31:07 +0800

    修改数据库

--- src/main/resources/sql/scutmall.sql
+++ src/main/resources/sql/scutmall.sql
@@ -263,7 +263,7 @@ CREATE TABLE `user` (
   `integration` int(11) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `phone` int(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
   `user_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
