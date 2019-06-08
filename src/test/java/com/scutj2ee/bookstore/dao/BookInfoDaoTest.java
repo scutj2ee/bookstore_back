2fb0b6f640c891184be653c3a4d5b75f2b806a06
1145054472@qq.com <1145054472@qq.com>
2019-06-08 21:20:16 +0800

    增加书本单元测试

--- src/test/java/com/scutj2ee/bookstore/dao/BookInfoDaoTest.java
+++ src/test/java/com/scutj2ee/bookstore/dao/BookInfoDaoTest.java
@@ -1,12 +1,15 @@
 package com.scutj2ee.bookstore.dao;
 
+import com.scutj2ee.bookstore.entity.BookInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -20,6 +23,37 @@ import java.util.List;
 public class BookInfoDaoTest {
     @Autowired
     private BookInfoDao bookInfoDao;
+
+    @Test
+    public void insertBookInfo(){
+        BookInfo bookInfo=new BookInfo();
+        bookInfo.setBookId(1);
+        bookInfo.setVersion("1");
+        bookInfo.setTranslator("1");
+        bookInfo.setPages(1);
+        bookInfo.setMemberPrice(1.0);
+        bookInfo.setIsShelf(1);
+        bookInfo.setStoreTime(new Date());
+        bookInfo.setStoreMount(1);
+        bookInfo.setSize("1");
+        bookInfo.setPublishDate(new Date());
+        bookInfo.setPrice(1.0);
+        bookInfo.setPress("1");
+        bookInfo.setPackStyle("1");
+        bookInfo.setOutline("1");
+        bookInfo.setName("1");
+        bookInfo.setMarketPrice(1.0);
+        bookInfo.setLookMount(1);
+        bookInfo.setIsbn("1");
+        bookInfo.setImageUrl("1");
+        bookInfo.setDetail("1");
+        bookInfo.setDiscount(BigDecimal.ONE);
+        bookInfo.setDealMount(1);
+        bookInfo.setCatalog("1");
+        bookInfo.setBookCategoryId(1);
+        bookInfo.setAuthor("1");
+        bookInfoDao.insertBookInfo(bookInfo);
+    }
 
     @Test
     public void getBookListParams() {
